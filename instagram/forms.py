86c7d03b993241eb95af2d2409566e0ac758f6ab
Julia Agasaro <agasarojulia@gmail.com>
2019-10-18 12:37:50 +0200

    Created registration

---
+++ instagram/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Profile,Image
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude=['user']
+
+
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        exclude =['likes','profile']
