e7cbc1e7d9408d53d351dccecb9925616c5717e5
Julia Agasaro <agasarojulia@gmail.com>
2019-10-18 12:35:54 +0200

    Added image and profile model

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,50 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Profile(models.Model):
+    profile_pic =models.ImageField(upload_to = 'media/')
+    bio = models.CharField(max_length=255)
+    user = models.OneToOneField(User,blank=True, on_delete=models.CASCADE, related_name="profile")
+
+    def __str__(self):
+        return str(self.bio)
+
+
+    def profile_save(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def get_by_id(cls, id):
+        profile = Profile.objects.get(owner=id)
+        return profile
+
+    @classmethod
+    def get_profile_by_username(cls, owner):
+        profiles = cls.objects.filter(owner__contains=owner)
+        return profiles
+class Image(models.Model):
+    pic= models.ImageField(upload_to = 'media/')
+    # pic=ImageField(manual_crop='1080x800', blank=True)
+    name= models.CharField(max_length=55)
+    caption = models.TextField(blank=True)
+    profile= models.ForeignKey(User, blank=True,on_delete=models.CASCADE)
+    profile_details = models.ForeignKey(Profile)
+
+    def __str__(self):
+        return str(self.name)
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def get_profile_images(cls, profile):
+        images = Image.objects.filter(profile__pk=profile)
+        return images
