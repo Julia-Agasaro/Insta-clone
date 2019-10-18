c53605863b8fd11b62cf2a3a902428cf54d00cbe
Julia Agasaro <agasarojulia@gmail.com>
2019-10-18 12:36:53 +0200

    Added home function and imported models

--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
+from django.http import Http404
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import Image, Profile
+from .forms import ProfileForm,ImageForm
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def home(request):
+    current_user = request.user
+    all_images = Image.objects.all()
+    return render(request,'home.html',locals())
+
+
