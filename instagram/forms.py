00bdb0b55e179b20bbadd3c85ee1841360f23e79
Julia Agasaro <agasarojulia@gmail.com>
2019-10-21 08:25:55 +0200

    Added a form for editing profile

--- instagram/forms.py
+++ instagram/forms.py
@@ -18,3 +18,10 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         exclude = ['image', 'comment_owner']
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('bio', 'profile_pic')
+        exclude = ['user']
